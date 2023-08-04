62d6bf9bc09d592b79fb2ce21062ff8a84df99ec
Chaitanya Padachh <chaitanyapadachh19@gmail.com>
2023-08-04 22:07:13 +0530

    Initial Commit

---
+++ todo_app/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path("", views.ListListView.as_view(), name="index"),
+    path("list/<int:list_id>/", views.ItemListView.as_view(), name="list"),
+]
