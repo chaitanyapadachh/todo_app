ec65738be3d3a89cff5ec594897db8613a59d653
Chaitanya Padachh <chaitanyapadachh19@gmail.com>
2023-08-05 11:57:05 +0530

    bugfix

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("todo_app.urls")),
-    path("accounts/",include("todo_app.urls"))
+    path("accounts/",include("todo_app.urls")),
+    path("utils/", include("utils.urls")),
 ]
